c087231d54898c2181918f272d74e09f74255f5c
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2022-06-27 13:32:36 -0700

    feat: initial version

---
+++ script.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * Enables use_squash_pr_title_as_default setting for repository
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {}
